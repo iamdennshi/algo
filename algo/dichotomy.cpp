22522cbbdae73cb83f98959ca50ec2bd31480a01
Den Shi <iamdennshi@gmail.com>
2023-03-04 15:19:22 +0500

    init

---
+++ algo/dichotomy.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+double fun(double x) {
+    return (-1 * x * x * x + 3 * (1 + x) * (std::log1p(x) - 1));
+}
+
+void dichotomy(double a, double b, double del, double eps)
+{
+    using namespace std;
+    double xk1, xk2;
+    int count = 0;
+
+    while (1) {
+        xk1 = (a + b) / 2 - del;
+        xk2 = (a + b) / 2 + del;
+        count++;
+        
+        if (fun(xk1) < fun(xk2)) {
+            b = xk2;
+        }
+        else {
+            a = xk1;
+        }
+
+        if (fabs(b - a) <= eps) {
+            double x = (a + b) / 2;
+            cout << "f(" << x << ") = " << fun(x) << endl;
+            cout << "Iteration: " << count << endl;
+            break;
+        }
+    }
+}
---
+++ algo/main.cpp
@@ -0,0 +1,8 @@
+void dichotomy(double a, double b, double del, double eps);
+
+int main() {
+	dichotomy(-0.5, 0.5, 1e-7, 1e-6);
+	//  f(2.38419e-07) = -3
+	//  Iteration: 21
+	return 0;
+}
